c4b63621d1d98bf44c53cae783280825780c2d7f
DoggerHou <mr.slipa3@mail.ru>
2023-05-13 20:07:43 +0500

    Добавьте файлы проекта.

---
+++ OS, Laba 4/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace OS__Laba_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(325, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "аваав";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(330, 287);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(293, 29);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Отменить махач за потоки";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 653);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ OS, Laba 4/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace OS__Laba_4
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        bool flag = true;
+        int[] array = new int[10];
+
+        public System.EventHandler obs;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+                array[i] = rnd.Next(0, 99);
+
+            label1.Text = Array_to_String(array);
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            flag = checkBox1.Checked;
+        }
+
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Thread inc = new Thread(IncreaseSort);
+            Thread dec = new Thread(DescendeSort);
+
+            //inc.Start();
+            dec.Start();
+            label1.Text = Array_to_String(array);
+        }
+
+
+
+        private string Array_to_String(int[] arr)
+        {
+            string s = "";
+            foreach (int i in arr)
+                s += i.ToString() + "  ";
+            return s;
+        }
+
+
+
+        private void DescendeSort()
+        {
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    if (array[i] > array[j])
+                    {
+                        (array[i], array[j]) = (array[j], array[i]);
+                        wait(200);
+                    }
+            //label1.Text = Array_to_String(array);
+        }
+
+
+        private void IncreaseSort()
+        {
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    if (array[i] < array[j])
+                    {
+                        (array[i], array[j]) = (array[j], array[i]);
+                        wait(200);
+                    }
+        }
+
+
+        public void wait(int milliseconds)
+        {
+            var timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+            };
+
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+        private void UpdateFromModel(object sender, EventArgs e)
+        {
+            label1.Text = Array_to_String(array);
+        }
+    }
+}
